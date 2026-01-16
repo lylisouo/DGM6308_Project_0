53e3baea0acb201afb0bb7846eb2304f1ad48fe3
lylisouo <kiracatcute@gmail.com>
2026-01-15 19:39:46 -0500

    Project 0

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the specific element
+const profilePic = document.getElementById("profile-pic");
+
+// Add click event listener
+profilePic.addEventListener("click", function () {
+    alert("Hello! Thanks for clicking my photo "); 
+});
